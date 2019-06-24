30f844ef9dbd7d54fa398bdfc6891af92a4c0ad9
Animadversio <lio50328@126.com>
2019-06-24 18:50:43 -0500

    Update flash_lag.m

--- flash_lag.m
+++ flash_lag.m
@@ -1,6 +1,10 @@
 %% Flash lag
-Width = 600; start_pos = [100, 200]; sq_size = 20; speed = [5,0]; 
-
+Width = 600; start_pos = [100, 200]; sq_size = 20; 
+speed = [5,0]; 
+flash_offset = [0,50];
+% Note the speed and offset could be tuned to probe where the percept of
+% the 2 images could align! 
+%%
 figure(1)
 img = 0.5 * ones(Width);
 imshow(img)
@@ -9,7 +13,9 @@ pos = start_pos;
 for t=1:80
     img_cur = insertShape(img, 'FilledRectangle', [pos(1) - sq_size/2, pos(2) - sq_size/2, sq_size, sq_size],'Color','black', 'Opacity', 1);
     if t == 40
-        img_cur = insertShape(img_cur, 'FilledRectangle', [pos(1) - sq_size/2, pos(2) + 50 - sq_size/2, sq_size, sq_size],'Color','red', 'Opacity', 1);
+        img_cur = insertShape(img_cur, 'FilledRectangle', [pos(1) + flash_offset(1) - sq_size/2, 
+                                                           pos(2) + flash_offset(2) - sq_size/2, 
+                                                           sq_size, sq_size],'Color','red', 'Opacity', 1);
     end
     pos = pos + speed;
     imshow(img_cur)
